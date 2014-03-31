100c47cc86569d67077e08332e9b17e5920816e5
Álvaro Alonso <aalonsog@dit.upm.es>
2014-03-31 09:50:06 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var config = {}
 
-config.idmURL = 'https://account.lab.fi-ware.eu';
+config.idmURL = 'https://account.lab.fi-ware.org';
 config.client_id = '';
 config.client_secret = '';
 config.callbackURL = 'http://localhost/login';
