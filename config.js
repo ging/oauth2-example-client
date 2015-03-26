1a2be0552e18acc8236ced45da629da0198ef105
aalonsog <aalonsog@dit.upm.es>
2015-03-26 16:59:40 +0100

    Added template

--- config.js
+++
@@ -1,8 +0,0 @@
-var config = {}
-
-config.idmURL = 'https://account.lab.fiware.org';
-config.client_id = '';
-config.client_secret = '';
-config.callbackURL = 'http://localhost/login';
-
-module.exports = config;
